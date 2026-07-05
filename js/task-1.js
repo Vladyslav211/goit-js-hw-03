baa54f7aee5f77a7d725062c362e900aedbe3958
Vladyslav Voloshyn <v.vlados211@gmail.com>
2026-07-05 18:21:43 +0200

    retry deploy

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
-function slugify(titel) {
-  return titel.toLowerCase().split(' ').join('-');
+function slugify(title) {
+  return title.toLowerCase().split(' ').join('-');
 }
 
 console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
